8ae1ffa2914746221fac606f03ef54db8ab7745c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-05 18:15:14 +0100

    adding temp

--- learn-more/use-API.py
+++ learn-more/use-API.py
@@ -4,9 +4,16 @@ import datetime as dt
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
 api_key = "5d6f9927daa5c67338d1fb97b7fad348"
 
+
+def kalvinToC_F(kalvin):
+    cels = kalvin - 275.15
+    fahr = cels * (9 / 5) + 32
+    return cels , fahr
+ 
+   
 city = input("enter the city you want :")
 url = BASE_URL + "appid=" + api_key + "&q=" + city
-
 response = requests.get(url).json()
 
+# kalvinTemp = response["main"]["temp"]
 print(response)
