b7794182ab3ef3710a99d52486f5c4a730d9f710
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-05 18:09:55 +0100

    doing the stuff

---
+++ learn-more/use-API.py
@@ -0,0 +1,12 @@
+import requests
+import datetime as dt
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+api_key = "5d6f9927daa5c67338d1fb97b7fad348"
+
+city = input("enter the city you want :")
+url = BASE_URL + "appid=" + api_key + "&q=" + city
+
+response = requests.get(url).json()
+
+print(response)
