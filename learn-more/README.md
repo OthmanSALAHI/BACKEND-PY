5e51949778227d2a6eaa102ae512fe7db48f03dd
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-05 13:27:14 +0100

    learn python more

---
+++ learn-more/README.md
@@ -0,0 +1,42 @@
+# To-Do List Manager
+
+This is a basic Python script for managing a to-do list. You can add, delete, and view tasks in a text file.
+
+## Usage
+
+1. Clone this repository or download the Python script.
+2. Open a terminal or command prompt.
+3. Navigate to the directory containing the Python script.
+4. Run the script with the following commands:
+
+   - To add a task:
+     ```
+     python script_name.py add "Task description"
+     ```
+
+   - To delete a task:
+     ```
+     python script_name.py del "Task description"
+     ```
+
+
+## Example
+
+Let's say you have the following tasks in your to-do list:
+
+- Buy groceries
+- Finish work project
+- Go for a run
+- Call mom
+
+To add a new task, run the following command:
+
+```
+python script_name.py add "Meet with a friend"
+```
+
+To delete a task, run the following command:
+
+```
+python script_name.py del "Go for a run"
+```
--- learn-more/To-Do.py
+++ learn-more/To-Do.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python3
 from sys import argv
 import os
-filename = __file__
-def TODO(command, todo_list, todo):
-    if command == "add":
-        todo_list.append(todo)
-    elif command == "del":
-        try:
-            todo_list.remove(todo)
-        except ValueError:
-            print(f"Todo '{todo}' not found in the list.")
-    else:
-        print("Command not found!")
 
-    for item in todo_list:
-        with open("todo.txt" , "a") as f:
-            f.write(f"  -- {item}\n")
+def add_task(filename, task):
+    with open(filename, 'a') as file:
+        file.write(f"{task}\n")
+
+def del_task(filename, task):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    
+    with open(filename, 'w') as file:
+        for line in lines:
+            if line.strip() != task:
+                file.write(line)
+
+def display_tasks(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        print(content)
 
 def main():
     if len(argv) < 3:
-        print(f"Usage: {filename} <command> <todo> <todo_list>")
+        print(f"Usage: {argv[0]} <command> <todo>")
         return
 
+    filename = "todo.txt"
     command = argv[1]
-    todo = argv[2]
-    todo_list = []
-    TODO(command, todo_list, todo)
-    with open("todo.txt","r") as file:
-        content = file.read()
-    print(content)
+    task = argv[2]
+
+    if command == "add":
+        add_task(filename, task)
+    elif command == "del":
+        del_task(filename, f"-- {task}")
+    else:
+        print("Command not found!")
+
+    display_tasks(filename)
+
 if __name__ == "__main__":
     main()
